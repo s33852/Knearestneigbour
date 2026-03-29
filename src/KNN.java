e9b020cc71a1e536666c6aea8beb5987df0c4b14
Julia <s33852@pjwstk.edu.pl>
2026-03-29 16:41:06 +0200

    m

--- src/KNN.java
+++ src/KNN.java
@@ -1,4 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import static java.lang.Math.sqrt;
+
 
 public class KNN {
     public static double dist(double[] arr1, double[] arr2) {
@@ -8,5 +15,30 @@ public class KNN {
         }
         return sqrt(dist);
     }
-    public static
+    public static List<DataPoint> loadData(File file) throws FileNotFoundException
+    {
+        List<DataPoint> data = new ArrayList<>();
+        Scanner scanner = new Scanner(file);
+
+        while(scanner.hasNextLine())
+        {
+            String line = scanner.nextLine().trim();
+            while(!line.isEmpty())
+            {
+                String[] parts = line.split(",");
+                double[] features = new double[parts.length - 1];
+
+                for (int i = 0; i < parts.length - 1; i++) {
+                    features[i] = Double.parseDouble(parts[i]);
+                }
+
+                String label = parts[parts.length - 1];
+                data.add(new DataPoint(features, label));
+            }
+
+        }
+        System.out.print("COS");
+        scanner.close();
+        return data;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File trainFile = new File("src/iris.data");
+        File testFile = new File("src/iris.test.data");
+        System.out.print("1");
+        List<DataPoint> trainSet = KNN.loadData(trainFile);
+        List<DataPoint> testSet = KNN.loadData(testFile);
+        
+        System.out.print("Cos");
+    }
 }
